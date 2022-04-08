52df4a539b8134c61b8fa2a498e5f7500876470d
Darren Snee <dsn@pml.ac.uk>
2022-04-08 10:20:41 +0100

    added conditional goggle avoidance in sitelist

--- www/js/includes/sitelist.js
+++ www/js/includes/sitelist.js
@@ -57,17 +57,18 @@ class SiteList {
 		thisMarkup += '</div>';
 		return( thisMarkup);
 	}
-
 	static getSiteMarkup( site, region, favorited=false) {
 		var thisItem = '';
 		thisItem += '<div class="card col-12 listColours" onclick="showSiteDetail('+site.id+','+region+')">';
 		thisItem +=  '<img class="card-img-top img-responsive">';
-		thisItem +=  '<div class="card-block">'
+		thisItem +=  '<div class="card-block card-sitelist">'
+		var avoidGogglesStyle = ""; // control right margin to keep text from overflowing goggles
 		if (( site.current_score ) && ( site.current_score > 0 )) {
 			var current_score = site.current_score;
 			if ( current_score > 5 ) {
 				current_score = 5;
 			}
+			avoidGogglesStyle = ' goggle-dodger';
 			var iconHeight = 48;
 			var iconWidth  = 96;
 			thisItem += '<div class="card-score">';
@@ -77,9 +78,9 @@ class SiteList {
 		if ( favorited) {
 			thisItem += '<div class="card-favorite"><img height="30" width="30" src="img/favorite.svg" /></div>';
 		}
-		thisItem += '   <h4 class="card-title">' + site.name +'</h4>';
-		thisItem += '   <p class="card-text">'   + site.description +'&nbsp;</p>'
-		thisItem += '   <p class="card-text">';
+		thisItem += '   <h4 class="card-title'+avoidGogglesStyle+'">' + site.name +'</h4>';
+		thisItem += '   <p class="card-text'+avoidGogglesStyle+'">'   + site.description +'&nbsp;</p>'
+		thisItem += '   <p class="card-text'+avoidGogglesStyle+'">';
 		thisItem += '<small class="text-muted">';
 		if ( site.score_time != null ) {
 			thisItem += 'Last updated ';
