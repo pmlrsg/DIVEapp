da7c8caef31a57d8f784a69e9e773ea3f6f2049c
dsn@pml.ac.uk <darren.snee@ingelby.com>
2023-03-29 23:16:08 +0100

    updated about page to reflect change from frappe-charts to charts.js
    also fixes e-shape logo placement and it's overlapping attribution links

--- www/js/includes/menubar.js
+++ www/js/includes/menubar.js
@@ -16,12 +16,12 @@ function getAboutContent() {
      <p>DIVE uses data from the EU Copernicus Marine Service to create a simple water visibility score for a dive site.</p><p>The DIVE app is currently in beta release and should not be used as the sole indicator of the suitability of a dive site.</p></p>DIVE is developed by the Centre for Geospatial Applications at <a href="https://pml.ac.uk">Plymouth Marine Laboratory</a> and we would welcome comments and suggestions for the improvement of the app. Please email us at <a href="mailto:eurogeoss@pml.ac.uk">eurogeoss@pml.ac.uk</a>.</p>
      <p>If you would like a specific dive site added to our system, feel free to email its details, along with it's latitude and longitude, to <a href="mailto:eurogeoss@pml.ac.uk?subject=Dive site addition request">eurogeoss@pml.ac.uk</a> for our consideration. Be aware that sites too close to the shore might not be suitable candidates due to land proximity confusing satellite sea surface observations.</p>
      <p>The DIVE app is built using these open source packages and services:
-<table><tr><td>Leaflet</td><td><a href="https://leafletjs.com/">https://leafletjs.com/</a></td></tr>
-<tr><td>frappe-charts</td><td><a href="https://frappe.io/charts">https://frappe.io/charts</a></td></tr>
+<table><tr><td>Leaflet</td><td><a href="https://leafletjs.com">https://leafletjs.com</a></td></tr>
+<tr><td>Chart.js</td><td><a href="https://chartjs.org">https://chartjs.org</a></td></tr>
 <tr><td>OpenStreetMap</td><td><a href="https://www.openstreetmap.org/about">https://www.openstreetmap.org/about</a></td></tr>
 </table></p>
      <p>Funded by</p>
-<div><a href="https://e-shape.eu/"><img class="dive-about-eshape-logo" src="img/e-shape-logo.png"></a></div>
+<div class="e-shape-logo-container"><a href="https://e-shape.eu/"><img class="dive-about-eshape-logo" src="img/e-shape-logo.png"></a></div>
   </div>
 </div>
 `;
